c164db0e1148570607e2971dc060ad1aac3397b8
cdowdy <cdowdy46@gmail.com>
2014-02-04 16:40:02 -0500

    hide and show javascript

---
+++ responsive_image/blocks/responsive_image/auto.js
@@ -0,0 +1,50 @@
+
+hideDiv = function() {
+	$("#first_query").hide();
+	$("#second_query").hide();
+	$("#third_query").hide();
+	$("#fourth_query").hide();
+	$("#custom_retina").hide();
+	$("#custom_large").hide();
+	$("#custom_medium").hide();
+	$("#custom_default").hide();
+};
+
+handleNewSelection = function() {
+
+	hideDiv();
+
+	switch ($(this).val()) {
+		case 'interchange':
+			$("#first_query").show();
+			$("#second_query").show();
+			$("#third_query").show();
+			$("#fourth_query").show();
+			break;
+		case 'foundation':
+			$("#first_query").show();
+			$("#second_query").show();
+			$("#third_query").show();
+			$("#fourth_query").show();
+			break;
+		case 'customQuery':
+			$("#custom_retina").show();
+			$("#custom_large").show();
+			$("#custom_medium").show();
+			$("#custom_default").show();
+			break;
+	}
+};
+
+$(document).ready(function() {
+	$("#mediaQuery").change(handleNewSelection);
+    
+    // Run the event handler once now to ensure everything is as it should be
+    handleNewSelection.apply($("#mediaQuery"));
+
+   $('#custom_div').hide();
+   $('#altChoice').change(function() {
+   ($(this).val() == "custom") ? $('#custom_div').show() : $('#custom_div').hide();
+  });
+});
+
